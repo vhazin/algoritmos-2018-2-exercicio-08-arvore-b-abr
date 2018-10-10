b36af3d2791f7021eac9a889ddf4ab0027e97df9
bernardo russo <bfcr@cesar.school>
2018-10-10 14:38:17 -0300

    Tentativa de usar nova lógica

--- main.c
+++ main.c
@@ -7,14 +7,52 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#define TRUE 1
+#define FALSE 0
+
+int leitura (){
+    int n;
+    scanf("%d",&n);
+    return n;
+}
+
+typedef struct btree{
+    int n;
+    int folhas;
+    
+}btree;
+
+int main (){
+    int n,add;
+    printf ("Digite o valor da ordem: ");
+    n=leitura();
+    printf ("\n Digite os valores a serem inseridos na arvore, digite 0 para finalizar entradas: ");
+    do{
+        scanf("%d",&add);
+        if (add == 0)
+            break;
+        
+    }while(1);
+    return 0;
+}
+
+/*
 typedef struct folha{
     int id;
-    int nivel;
-    struct folha *direita;
-    struct folha *esquerda;
+    int altura;
+    int ordem;
+    struct no *direita;
+    struct no *esquerda;
     struct folha *raiz;
     int idfolha;
 }folha;
+
+
+typedef struct no{
+    folha *esquerda;
+    folha *direita;
+}no;
 
 typedef struct arvb{
     int tamanho;
@@ -42,4 +80,4 @@ int main() {
     scanf("%d",&n);
     
     return 0;
-}
+}*/
